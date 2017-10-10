44aa130f6359999b40698e92919b59063910842d
Wessel Klijnsma <wesselklijnsma@gmail.com>
2017-10-10 14:31:15 +0200

    init

--- docs/visualization.js
+++ docs/visualization.js
@@ -36,7 +36,7 @@ function initMap() {
         markers.push(marker);
         marker.addListener('click', function () {
             infowindow.open(map, marker);
-            $.get("https://anyorigin.com/go?url=" +
+            $.get("https://allorigins.us/get?url=" +
                 encodeURIComponent("https://www.marinetraffic.com/en/ais/details/ships/mmsi:") + ship.mmsi,
                 function (data) {
                     var elements = $("<div>").html(data)[0].$("center-block");
